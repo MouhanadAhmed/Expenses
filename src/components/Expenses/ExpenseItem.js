14a22c7b3b9e56e0306b2d7ca128e286a299f813
MouhaNad-ux <83966010+MouhaNad-ux@users.noreply.github.com>
2021-06-04 18:47:09 +0200

    Second commit

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
@@ -7,25 +7,15 @@ import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
 
-const [title, setTitle] =  useState(props.title);
-console.log('ExpenseItem Evaluated by React');
-  
 
-
- const clickHandler =() => {
-   setTitle('Updated!');
-  console.log(title);
-   
- };
 
   return (
     <Card className='expense-item'>
       <ExpenseDate date={props.date} />
       <div className='expense-item__description'>
-        <h2>{title }</h2>
+        <h2>{props.title }</h2>
         <div className='expense-item__price'>${props.amount}</div>
       </div>
-      <button onClick={clickHandler} >Change Title</button>
     </Card>
   );
 }
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
 import './Expenses.css';
+import ExpensesFilter from './ExpensesFilter';
 
 
-function Expenses(props) {
+const Expenses = (props) => {
+
+  const [filteredYear, setFilteredYear ] = useState('2020');
+
+  const filterChangeHandler = selectedYear => {
+    setFilteredYear(selectedYear);
+  };
   return (
     <Card className="expenses">
+      <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
       <ExpenseItem
         title={props.items[0].title}
         amount={props.items[0].amount}
@@ -27,6 +35,7 @@ function Expenses(props) {
         amount={props.items[3].amount}
         date={props.items[3].date}
       />
+      
     </Card>
   );
 }
